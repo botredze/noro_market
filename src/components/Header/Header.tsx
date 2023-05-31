ae2dba1df2c0fefcb3d724f0d9d07737b1f331c8
botredze <botedze@gmail.com>
2023-06-01 01:55:35 +0600

    ref

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,36 +2,14 @@ import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
 import { Menu as MenuIcon } from '@material-ui/icons';
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import style from "../Header/Header.module.css";
 
-const useStyles = makeStyles((theme) => ({
-    appBar: {
-        backgroundColor: '#ffffff',
-        color: theme.palette.primary.main,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-        fontWeight: 'bold',
-        fontSize: '1.5rem',
-    },
-}));
-
-const Header: React.FC = () => {
-    const classes = useStyles();
+const Header = () => {
 
     return (
-        <AppBar position="fixed" className={classes.appBar}>
-            <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                    NoraMarket
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <div>
+            <p className={style.logo}>Nopa <br />Market</p>
+        </div>
     );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,98 +5,11 @@ import Footer from "@/components/Footer/Footer";
 
 const HomePage: NextPage = () => {
     return (
-        <Box sx={{ backgroundColor: '#f2f2f2', minHeight: '100vh' }}>
-            <Header />
-            <Box sx={{ backgroundColor: '#f6f6f6', height: '40vh' }} />
 
-            <Container maxWidth="lg" sx={{ py: 4 }}>
-                <Box sx={{ display: 'flex', marginBottom: 4 }}>
-                    <Typography variant="h4" component="h1" sx={{ fontWeight: 'bold' }}>
-                        Our Products
-                    </Typography>
-
-                    <Box sx={{ flexGrow: 1 }} />
-
-                    <Button variant="contained" color="primary">View All</Button>
-                </Box>
-
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
-                    {/*<Card sx={{ maxWidth: 345, marginBottom: 3 }}>*/}
-                    {/*    <CardActionArea>*/}
-                    {/*        <CardMedia component="img" height="140" image="/product-1.jpg" alt="Product 1" />*/}
-                    {/*        <CardContent>*/}
-                    {/*            <Typography gutterBottom variant="h5" component="h2">*/}
-                    {/*                Product 1*/}
-                    {/*            </Typography>*/}
-                    {/*            <Typography variant="body2" color="textSecondary" component="p">*/}
-                    {/*                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.*/}
-                    {/*            </Typography>*/}
-                    {/*        </CardContent>*/}
-                    {/*    </CardActionArea>*/}
-                    {/*    <CardActions>*/}
-                    {/*        <Button size="small">Learn More</Button>*/}
-                    {/*        <Button size="small" color="primary">Buy Now</Button>*/}
-                    {/*    </CardActions>*/}
-                    {/*</Card>*/}
-
-                    {/*<Card sx={{ maxWidth: 345, marginBottom: 3 }}>*/}
-                    {/*    <CardActionArea>*/}
-                    {/*        <CardMedia component="img" height="140" image="/product-2.jpg" alt="Product 2" />*/}
-                    {/*        <CardContent>*/}
-                    {/*            <Typography gutterBottom variant="h5" component="h2">*/}
-                    {/*                Product 2*/}
-                    {/*            </Typography>*/}
-                    {/*            <Typography variant="body2" color="textSecondary" component="p">*/}
-                    {/*                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.*/}
-                    {/*            </Typography>*/}
-                    {/*        </CardContent>*/}
-                    {/*    </CardActionArea>*/}
-                    {/*    <CardActions>*/}
-                    {/*        <Button size="small">Learn More</Button>*/}
-                    {/*        <Button size="small" color="primary">Buy Now</Button>*/}
-                    {/*    </CardActions>*/}
-                    {/*</Card>*/}
-
-                    {/*<Card sx={{ maxWidth: 345, marginBottom: 3 }}>*/}
-                    {/*    <CardActionArea>*/}
-                    {/*        <CardMedia component="img" height="140" image="/product-3.jpg" alt="Product 3" />*/}
-                    {/*        <CardContent>*/}
-                    {/*            <Typography gutterBottom variant="h5" component="h2">*/}
-                    {/*                Product 3*/}
-                    {/*            </Typography>*/}
-                    {/*            <Typography variant="body2" color="textSecondary" component="p">*/}
-                    {/*                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.*/}
-                    {/*            </Typography>*/}
-                    {/*        </CardContent>*/}
-                    {/*    </CardActionArea>*/}
-                    {/*    <CardActions>*/}
-                    {/*        <Button size="small">Learn More</Button>*/}
-                    {/*        <Button size="small" color="primary">Buy Now</Button>*/}
-                    {/*    </CardActions>*/}
-                    {/*</Card>*/}
-
-                    {/*<Card sx={{ maxWidth: 345, marginBottom: 3 }}>*/}
-                    {/*    <CardActionArea>*/}
-                    {/*        <CardMedia component="img" height="140" image="/product-4.jpg" alt="Product 4" />*/}
-                    {/*        <CardContent>*/}
-                    {/*            <Typography gutterBottom variant="h5" component="h2">*/}
-                    {/*                Product 4*/}
-                    {/*            </Typography>*/}
-                    {/*            <Typography variant="body2" color="textSecondary" component="p">*/}
-                    {/*                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.*/}
-                    {/*            </Typography>*/}
-                    {/*        </CardContent>*/}
-                    {/*    </CardActionArea>*/}
-                    {/*    <CardActions>*/}
-                    {/*        <Button size="small">Learn More</Button>*/}
-                    {/*        <Button size="small" color="primary">Buy Now</Button>*/}
-                    {/*    </CardActions>*/}
-                    {/*</Card>*/}
-                </Box>
-
-                <Footer />
-            </Container>
-        </Box>
+    <div>
+        <Header/>
+        Привет !
+    </div>
     );
 };
 
